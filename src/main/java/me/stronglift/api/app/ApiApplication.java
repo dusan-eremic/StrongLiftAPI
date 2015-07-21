3831a8f792ae697e3b3a3898c3b2517a85fa2e85
Dusan Eremic <d.eremic@gmail.com>
2015-07-21 14:32:30 +1200

    Added some Serbian JavaDoc

--- src/main/java/me/stronglift/api/app/ApiApplication.java
+++ src/main/java/me/stronglift/api/app/ApiApplication.java
@@ -11,25 +11,37 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 /**
- * Jersey application setup.
+ * Jersey application configuration.
  * 
- * @author Dusan Eremic TODO - complete JavaDoc
+ * @author Dusan Eremic
  *
  */
 public class ApiApplication extends ResourceConfig {
 	
+	/**
+	 * Default constructor koji koristi IN_MEMORY service factory
+	 */
 	public ApiApplication() {
 		this(ServiceFactory.get(FactoryImpl.IN_MEMORY));
 	}
 	
+	/**
+	 * Constructor koji prihvata instancu service factory-ja
+	 * 
+	 * @param serviceFactory Service factory koje će biti korišćen za dependency
+	 *            injection na svim mestima gde se traži ServiceFactory.class
+	 */
 	public ApiApplication(final ServiceFactory serviceFactory) {
 		
+		// Registracija paketa koji će biti skenirani za @Provider anotaciju
 		packages("me.stronglift.api.controller");
 		packages("me.stronglift.api.filter");
 		packages("me.stronglift.api.error");
 		packages("me.stronglift.api.model");
 		packages("me.stronglift.api.controller.resource");
 		
+		// Registracija prosleđenog service factory-ja koji
+		// će biti korišćen za dependency injection
 		register(new AbstractBinder() {
 			
 			@Override
@@ -39,11 +51,12 @@ public class ApiApplication extends ResourceConfig {
 			
 		});
 		
+		// Zamena defaultnog Json parsera za Jackson
 		final JacksonJsonProvider json = new JacksonJsonProvider();
 		json.configure(SerializationFeature.INDENT_OUTPUT, true);
 		register(json);
 		
-		property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true); // Sends an error message inside the response body
+		property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
 	}
 	
 }
--- src/main/java/me/stronglift/api/controller/BaseController.java
+++ src/main/java/me/stronglift/api/controller/BaseController.java
@@ -15,26 +15,45 @@ import me.stronglift.api.model.User;
 import me.stronglift.api.service.ServiceFactory;
 
 /**
- * The base controller implementing common methods and it will be extended by all other controllers.
+ * Base Jersey kontroler sa zajedničkim metodama.
  * 
  * @author Dušan Eremić
  *
  */
 public abstract class BaseController {
 	
+	/**
+	 * Key za User objekat smešten u ContainerRequestContext
+	 */
 	public static final String USER = "User";
 	
+	/**
+	 * Injected service factory
+	 */
 	@Context
 	protected ServiceFactory serviceFactory;
 	
+	/**
+	 * Injected UriInfo
+	 */
 	@Context
 	protected UriInfo uriInfo;
 	
+	/**
+	 * User koji je napravio request
+	 */
 	protected User user;
 	
+	/**
+	 * Injected request context
+	 */
 	@Context
 	private ContainerRequestContext request;
 	
+	/**
+	 * Metoda koja će biti pozvana nakon konstruktora i koja će uzeti User-a iz
+	 * request contexta i smestiti ga u class field.
+	 */
 	@PostConstruct
 	private void setUser() {
 		if (request.getProperty(USER) != null) {
@@ -42,12 +61,29 @@ public abstract class BaseController {
 		}
 	}
 	
+	/**
+	 * Metoda kreira "created" response
+	 * 
+	 * @param entity Entitet koji je kreiran.
+	 * @return HTTP response 201 koji sadrži kreirani entitet i resource path u
+	 *         HTTP header-u.
+	 */
 	protected <T extends BaseEntity<T>> Response created(T entity) {
-		URI path = uriInfo.getAbsolutePathBuilder().path(entity.getId()).build();
-		return Response.created(path).entity(new Resource<>(uriInfo, LinkType.SELF, entity)).build();
+		URI path = uriInfo.getAbsolutePathBuilder().path(entity.getId())
+				.build();
+		return Response.created(path)
+				.entity(new Resource<>(uriInfo, LinkType.SELF, entity)).build();
 	}
 	
+	/**
+	 * Metoda kreira "updated" response
+	 * 
+	 * @param entity Entitet koji je ažuriran.
+	 * @return HTTP response 200 koji sadrži ažurirani entiti i resource path u
+	 *         HTTP header-u.
+	 */
 	protected <T extends BaseEntity<T>> Response updated(T entity) {
-		return Response.ok().entity(new Resource<>(uriInfo, LinkType.SELF, entity)).build();
+		return Response.ok()
+				.entity(new Resource<>(uriInfo, LinkType.SELF, entity)).build();
 	}
 }
